8b255e42c4fe3e799d073a6b899d5261c482cb2d
aaroncalero <aaron.calero1@educa.madrid.org>
2024-11-26 14:22:19 +0100

    js

---
+++ js/ejemplo1.1.js
@@ -0,0 +1,15 @@
+var nombre = prompt("Dime tu nombre");
+var instituto = prompt("Nombre de instituto")
+console.log(nombre);
+alert(nombre);
+document.write("<h1>" + nombre + "</h1>")
+document.write("<table border =\"1\">")
+document.write("<tr><th>Nombre</th><th>Instituto</th></tr>")
+document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr>")
+document.write("</table>")
+
+
+var nota1 = 5;
+var nota2 = 4;
+var notaTotal = nota1 + nota2;
+var notaMedia = (nota1 + nota2)
---
+++ js/ejemplo1.js
@@ -0,0 +1,20 @@
+console.log; ('prueba de código');
+
+function imprimirMensaje(mensaje) {
+    console.log(mensaje);
+    //alert(mensaje);
+    console.log("Hola mundo")
+}
+
+function tipoDeVariable(mivariable) {
+    console.log("Mi variable es de tipo:" + typeof (mivariable));
+    console.log(mensaje)
+}
+var alumnoFila2 = ["Jacob", "Aleks", "David", "Jorge"];
+tipoDeVariable(alumnoFila2);
+
+var notaExamen3s = 6;
+tipoDeVariable(notaExamen3s)
+
+var miNombre ="Sonia";
+tipoDeVariable(miNombre)
