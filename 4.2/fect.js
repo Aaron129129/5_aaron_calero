be9aa711ddf5ce02e9a2158c2ca26028c8b61f05
aaroncalero <aaron.calero1@educa.madrid.org>
2024-12-03 13:22:44 +0100

    4.2 prime

---
+++ 4.2/fect.js
@@ -0,0 +1,21 @@
+function calcularNotaFinal() {
+    var notaPrimeraEv = document.getElementById("nota_primera_ev").value;
+    console.log("Nota primer input: " + notaPrimeraEv);
+    
+    var notaSegundaEv = document.getElementById("nota_segunda_ev").value;
+    console.log("Nota segundo input: " + notaSegundaEv);
+    
+    var nombre = document.getElementById("nombre").value;
+    
+    var divResultado = document.getElementById("resultado");
+    console.log("Nota media");
+    
+    var notaFinal = (parseInt(notaPrimeraEv) + parseInt(notaSegundaEv)) / 2;
+    if (notaFinal >= 5) {
+        divResultado.innerHTML = "APROBADO ;)" + nombre + "Tu nota es: " + notaFinal;
+        divResultado.style.backgroundColor = "green";
+    } else {
+        divResultado.innerHTML = "VUELVE A INTENTARLO :(" + nombre + "Tu nota es: " + notaFinal;
+        divResultado.style.backgroundColor = "red";
+    }
+}
